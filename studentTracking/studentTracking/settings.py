199ca23f4fb0b64f04e6ea6627d8addc81a3c6ea
SithabileM <sithabilemonde9@gmail.com>
2025-08-22 14:23:15 +0200

    add static root in settings.py

--- studentTracking/studentTracking/settings.py
+++ studentTracking/studentTracking/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os 
 import os
+from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -117,6 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = 'static/'
 
 # Default primary key field type
