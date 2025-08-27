de13db84b34565394c8dd2be9cc867e7d517ca12
SithabileM <sithabilemonde9@gmail.com>
2025-08-27 13:55:39 +0200

    style the index page

--- studentTracking/studentTracking/settings.py
+++ studentTracking/studentTracking/settings.py
@@ -95,7 +95,11 @@ if DEBUG==True:
 DATABASE_URL=os.environ.get("DATABASE_URL")
 
 DATABASES = {
-    'default': dj_database_url.parse(DATABASE_URL,conn_max_age=600)
+    'default': dj_database_url.config(
+        env="DATABASE_URL",
+        conn_max_age=600,
+        ssl_require=True
+        )
 }
 
 # Password validation
